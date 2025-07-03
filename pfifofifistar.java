991752846669521f34a53cdcbcf128599f9beb98
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 19:49:59 +0530

    Create pfifofifistar.java
    create

---
+++ pfifofifistar.java
@@ -0,0 +1,23 @@
+class pfifofifistar
+{
+	public static void main (String arg[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("=");
+			}
+			for(int k=5; k>=1; k--)
+			{
+				System.out.print("+");
+			}
+		System.out.println();
+		}
+	}
+}
+//=====+++++
+//====+++++
+//===+++++
+//==+++++
+//=+++++
