196243215425d2b3315641b0f06cf49e31402260
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 19:41:47 +0530

    Create Star55Line.java
    create

---
+++ Star55Line.java
@@ -0,0 +1,21 @@
+class Star55Line
+{
+    public static void main(String arg[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=5; j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+				}
+	}
+}
+
+//Output
+//*****
+//*****
+//*****
+//*****
+//*****
