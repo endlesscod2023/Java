3bc7c2bd27af21bed030bdbb4c63b48b625054ca
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 19:37:14 +0530

    Create fivestarttb.java
    create

---
+++ fivestarttb.java
@@ -0,0 +1,17 @@
+public class fivestarttb
+{
+    public static void main (String[] args)
+	{
+		for (int i=1; i<=5; i++)
+		{
+			System.out.println("*");
+		}
+	}
+}
+
+//Output 
+//*
+//*
+//*
+//*
+//*
