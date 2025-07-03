2ea543902fb524bc049d5cb2c712fa4c1144fa60
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 19:56:47 +0530

    Create fitoonstar.java
    create

---
+++ fitoonstar.java
@@ -0,0 +1,24 @@
+class fitoonstar
+{
+    public static void main(String arg[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+		}
+	}
+}
+/*
+
+Output
+*****
+****
+***
+**
+*
+
+*/
