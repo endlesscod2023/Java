b5e8def5faf2388375a1604dad5881731b3a4670
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 20:02:47 +0530

    Create pfifo0ntoninestar.java
    create

---
+++ pfifo0ntoninestar.java
@@ -0,0 +1,28 @@
+class pfifo0ntoninestar
+{
+	public static void main (String arg[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("=");
+			}
+			for(int k=1; k<=i*2-1; k++)
+			{
+				System.out.print("+");
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+
+Output
+=====+
+====+++
+===+++++
+==+++++++
+=+++++++++
+
+*/
