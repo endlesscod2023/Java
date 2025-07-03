33dec3f203c7ae02efbe5a5aa15602eafb3dbdf3
endlesscod2023 <endlesscod2023@gmail.com>
2025-07-03 20:09:46 +0530

    Create p5to1S1to9P1to5S9to1star.java
    create

---
+++ p5to1S1to9P1to5S9to1star.java
@@ -0,0 +1,47 @@
+class p5to1S1to9P1to5S9to1star
+{
+	public static void main (String arg[])
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=5; j>=i; j--)
+			{
+				System.out.print("=");
+			}
+			for(int k=1; k<=i*2-1; k++)
+			{
+				System.out.print("+");
+		}
+		System.out.println();
+		}
+		for(int l=1; l<=5; l++)
+		{
+			for(int m=1; m<=l; m++)
+			{
+				System.out.print("=");
+			}
+			for(int n=9; n>=l*2-1; n--)
+			{
+				System.out.print("+");
+			}
+			System.out.println();
+		
+		}
+	}
+}
+/*
+
+Output
+=====+
+====+++
+===+++++
+==+++++++
+=+++++++++
+=+++++++++
+==+++++++
+===+++++
+====+++
+=====+
+
+*/
+
